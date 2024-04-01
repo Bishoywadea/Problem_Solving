05928d811230dc60d11dad059a74d30b2c2af2ed
-NoName <108888519+Bishoywadea@users.noreply.github.com>
2024-04-01 03:16:19 +0200

    Time: 4 ms (47.87%) | Memory: 7.6 MB (57.50%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i=s.size()-1;
+        int ans=0;
+        while(i>=0 && s[i] == ' ') i--;
+        while(i>=0 && s[i] != ' ') i--,ans++;
+        return ans;
+    }
+};
