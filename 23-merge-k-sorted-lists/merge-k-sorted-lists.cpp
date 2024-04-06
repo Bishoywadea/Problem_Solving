15d127babf04259f662b88dd64b4bcfe5b3134f9
-NoName <108888519+Bishoywadea@users.noreply.github.com>
2024-04-06 18:30:13 +0200

    Time: 16 ms (64.32%) | Memory: 16.9 MB (76.69%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,35 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* ans=new ListNode(0);
-        ListNode* cur=ans;
-        int mn;
-        // bool f=1;
-        while(1){
-            int j=0;
-            while(j<lists.size() && !lists[j]) j++;
-            if(j==lists.size()) return ans->next;
-            mn=j;
-            // f=0;
-            for(int i=j+1;i<lists.size();i++){
-                // if(lists[i]) f=1;
-                if(lists[i] && lists[i]->val<lists[mn]->val) mn=i;
-            }
-            // cout<<lists[mn]->val<< " "<<endl;
-            cur->next = lists[mn];
-            cur=cur->next;
-            lists[mn] = lists[mn]->next;
+    struct compare {
+        bool operator()(const ListNode* l, const ListNode* r) {
+            return l->val > r->val;
         }
-        return ans->next;
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        for(int i = 0; i < lists.size(); i++){
+            if(lists[i])
+                pq.push(lists[i]);
+        }
+
+        ListNode* head = new ListNode(-1);
+        ListNode* temp = head;
+        while(pq.size() > 0){
+            ListNode* smallest = pq.top();
+            pq.pop();
+            temp->next = smallest;
+            temp = temp->next;
+
+            if(smallest->next)
+                pq.push(smallest->next);
+        }
+        return head->next;
     }
 };
