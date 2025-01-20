f1ae4319cb0083477991bb812ff930e7df6bfc9e
-NoName <108888519+Bishoywadea@users.noreply.github.com>
2025-01-20 02:35:25 +0200

    Time: 7 ms (89.94%) | Memory: 133.7 MB (95.12%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -4,15 +4,13 @@ public:
         vector<int> r(mat.size(),mat[0].size());
         vector<int> c(mat[0].size(),mat.size());
 
-        unordered_map<int,pair<int,int>> m;
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat[0].size();j++){
-                m[mat[i][j]]={i,j};
-            }    
-        }
+        vector<pair<int, int>> m(mat.size()*mat[0].size());
+        for (int i = 0; i < mat.size(); i++)
+            for (int j = 0; j < mat[0].size(); j++)
+                m[mat[i][j]-1] = {i, j};
         int index=0;
         for(auto i:arr){
-            pair<int,int> cor = m[i];
+            pair<int,int> cor = m[i-1];
             r[cor.first]--;
             c[cor.second]--;
             if(!c[cor.second] || !r[cor.first])return index;
