e3f1630781d4d1eb28c24d1a3c0c42c2132758e8
-NoName <108888519+Bishoywadea@users.noreply.github.com>
2025-02-18 19:03:34 +0200

    Time: 0 ms (100.00%) | Memory: 8.3 MB (35.90%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,47 +1,18 @@
 class Solution {
 public:
-    vector<int> rec(vector<int> cur, vector<bool> taken, string pattern, int index){
-        if(index>=pattern.size()) return cur;
-        int n = cur.size();
-        if(pattern[index] == 'I'){
-            for(int i = cur[n-1]+1 ; i<taken.size();i++){
-                if(!taken[i]){
-                    cur.push_back(i);
-                    taken[i] = 1; 
-                    vector<int> ans = rec(cur,taken, pattern, index+1);
-                    if(ans.size()) return ans;
-                    taken[i]=0;
-                    cur.pop_back();
-                }
-            }
-        }
-        else{
-            for(int i = cur[n-1]-1 ; i>=1;i--){
-                if(!taken[i]){
-                    cur.push_back(i);
-                    taken[i] = 1; 
-                    vector<int> ans = rec(cur,taken, pattern, index+1);
-                    if(ans.size()) return ans;
-                    taken[i]=0;
-                    cur.pop_back();
-                }
-            }
-        }
-        return {};
-    }
     string smallestNumber(string pattern) {
-        vector<int> ans;
-        vector<bool> taken(10,0);
-        for(int i=1;i<=9;i++){
-            taken[i]=1;
-            ans = rec({i}, taken, pattern, 0);
-            if(ans.size()) {
-                string s = "";
-                for(auto i:ans) s.push_back(i+'0');
-                return s;
+        string result;
+        stack<int> st;
+
+        for (int i = 0; i <= pattern.size(); i++) {
+            st.push(i + 1);
+            if (i == pattern.size() || pattern[i] == 'I') {
+                while (!st.empty()) {
+                    result += to_string(st.top());
+                    st.pop();
+                }
             }
-            taken[i]=0;
         }
-        return "";
+        return result;
     }
 };
