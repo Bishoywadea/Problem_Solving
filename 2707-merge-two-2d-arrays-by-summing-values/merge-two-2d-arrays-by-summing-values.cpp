6edb9028b639b4e6612cfb958b60e608ac43f38c
-NoName <108888519+Bishoywadea@users.noreply.github.com>
2025-03-02 14:31:01 +0200

    Time: 0 ms (100.00%) | Memory: 14.5 MB (95.86%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -4,21 +4,12 @@ public:
         vector<vector<int>> ans;
         int i = 0, j=0;
         while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i][0] < nums2[j][0]){
-                ans.push_back(nums1[i++]);
-            }
+            if(nums1[i][0] < nums2[j][0]) ans.push_back(nums1[i++]);
             else if(nums2[j][0] < nums1[i][0]) ans.push_back(nums2[j++]);
             else ans.push_back({nums2[j][0],nums1[i++][1] + nums2[j++][1]});
         }
-
-        while(i<nums1.size()){
-            ans.push_back(nums1[i++]);
-        }
-
-        while(j<nums2.size()){
-            ans.push_back(nums2[j++]);
-        }
-
+        while(i<nums1.size()) ans.push_back(nums1[i++]);
+        while(j<nums2.size()) ans.push_back(nums2[j++]);
         return ans;
     }
 };
